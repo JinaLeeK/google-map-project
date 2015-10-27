1f34d4b664f6c5bfe74ac9d163a9d98870d00420
JinaLeeK <grissom1015@gmail.com>
2015-10-27 00:48:21 -0400

    access yelp API test

---
+++ server-client-demo/form-demo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-type" content="text/html;charset=utf-8">
+  </head>
+  <body>
+    <?php
+    echo 'test';
+      // echo $_REQUEST['nickname'].' your job: '.$_REQUEST['job'].'.'
+     ?>
+  </body>
+</html>
