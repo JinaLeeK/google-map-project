50c516699a9a577375e3a397cf0845edcc638ddc
JinaLeeK <grissom1015@gmail.com>
2016-08-09 10:38:11 -0400

    santi

--- config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
- echo json_encode(array('consumerKey'=>'8JG3jYSS0qz9ysi826Ha6Q',
-          'consumerSecret'=>'mhYhjI6e0t8cKNReiwhcP4UNVe8',
-          'accessToken'=>'8taFgd1FgD41cJo1eEUSIWoZVtFvtYcp',
-          'accessTokenSecret'=>'H5fuG8jCvJDBxEFgPOVecoAeU_M'));
-?>
